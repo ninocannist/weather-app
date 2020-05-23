afce3e28dfb172176e3db575b3441bd9df49778d
Antonino Cannistraci <nino.cannist@gmail.com>
2020-05-23 13:27:06 +0200

    Server setup

---
+++ public/build/precache-manifest.4f52fd2d36a0db0c026b9930d161472c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb0057d8bae751059d7c8b14c345c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b43464617589837f0180",
+    "url": "/static/css/main.1a7ff3fe.chunk.css"
+  },
+  {
+    "revision": "3a347a6c71bc5837dd86",
+    "url": "/static/js/2.13684c92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13684c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43464617589837f0180",
+    "url": "/static/js/main.f8c8c5be.chunk.js"
+  },
+  {
+    "revision": "5cfe63f3d0aa65dc782b",
+    "url": "/static/js/runtime-main.dbafcd4d.js"
+  }
+]);
--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 // Allow cross-origin
 app.use(cors());
 
-app.use(express.static('public'));
+app.use(express.static('build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
